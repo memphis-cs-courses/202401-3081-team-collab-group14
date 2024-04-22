e0c217bb92e45ecb712e9565c6d6961f9834ace0
Luis Perez <luisperez07182@email.com>
2024-04-22 15:07:32 -0500

    Add username page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get 'amaerlide', to: 'pages#amaerlide', as: 'amaerlide'
+
 end
